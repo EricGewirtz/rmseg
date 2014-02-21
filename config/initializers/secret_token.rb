a9bfc37731459a62460e9f87b2040779a3b7ed5f
Eric Gewirtz <eric.gewirtz@avaion.com>
2014-02-21 00:31:14 -0500

    fixed .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rmseg::Application.config.secret_key_base = '1238c621810c5d346afc00ce2cbae05cf3ed7c66c1c7a33a8119ef256071252359fc7d47f6464797d07bb16a3a1d5a372d4fcf801fed8af815b9ceee1caf22e7'
